c018c80cee55b260a9abed776772501b7f4e3bac
Luan Fernandes <souluanf@icloud.com>
2025-02-06 18:46:43 -0300

    feat(controller): adicionar TransactionController

---
+++ backend/src/main/java/dev/luanfernandes/controller/TransactionController.java
@@ -0,0 +1,255 @@
+package dev.luanfernandes.controller;
+
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTIONS_GROUPED_V1;
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTIONS_V1;
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTION_CPF_V1;
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTION_PROCESS_FILE_V1;
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTION_STORE_BALANCE_V1;
+import static dev.luanfernandes.domain.constants.PathConstants.TRANSACTION_STORE_NAME_V1;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+import dev.luanfernandes.domain.response.BalanceResponse;
+import dev.luanfernandes.domain.response.ProcessFileResponse;
+import dev.luanfernandes.domain.response.StoreResponse;
+import dev.luanfernandes.domain.response.TransactionResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping(produces = APPLICATION_JSON_VALUE)
+public interface TransactionController {
+
+    @Operation(
+            summary = "Process a file with transactions",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Ok"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @PostMapping(value = TRANSACTION_PROCESS_FILE_V1, consumes = MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<ProcessFileResponse> processFile(@RequestParam("file") MultipartFile file);
+
+    @Operation(
+            summary = "Get transactions grouped by store",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "OK"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @GetMapping(TRANSACTIONS_GROUPED_V1)
+    ResponseEntity<List<StoreResponse>> getTransactionsGrouped();
+
+    @Operation(
+            summary = "Get all transactions",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "OK"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @GetMapping(TRANSACTIONS_V1)
+    ResponseEntity<List<TransactionResponse>> getTransactions();
+
+    @Operation(
+            summary = "Get transactions by store",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "OK"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @GetMapping(TRANSACTION_STORE_NAME_V1)
+    ResponseEntity<List<TransactionResponse>> getTransactionsByStore(@PathVariable String name);
+
+    @Operation(
+            summary = "Get all stores balances",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "OK"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @GetMapping(TRANSACTION_STORE_BALANCE_V1)
+    ResponseEntity<List<BalanceResponse>> getStoresBalances();
+
+    @Operation(
+            summary = "Find all transactions by CPF",
+            tags = {"transactions"},
+            responses = {
+                @ApiResponse(responseCode = "200", description = "OK"),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Bad Request",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Not Found",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "422",
+                        description = "Unprocessable Entity",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Internal Server Error",
+                        content =
+                                @Content(
+                                        mediaType = APPLICATION_PROBLEM_JSON_VALUE,
+                                        schema = @Schema(implementation = ProblemDetail.class)))
+            })
+    @GetMapping(TRANSACTION_CPF_V1)
+    ResponseEntity<List<TransactionResponse>> getTransactionsByCPF(@PathVariable String cpf);
+}
