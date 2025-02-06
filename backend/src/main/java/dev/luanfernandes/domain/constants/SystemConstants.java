453bce8d8786dc0e98b28f153d93761c6f0d4276
Luan Fernandes <souluanf@icloud.com>
2025-02-06 20:10:43 -0300

    feat(domain): adicionar SystemConstants

---
+++ backend/src/main/java/dev/luanfernandes/domain/constants/SystemConstants.java
@@ -0,0 +1,9 @@
+package dev.luanfernandes.domain.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SystemConstants {
+    public static final String ENTRADA = "Entrada";
+    public static final String SAIDA = "Saída";
+}
